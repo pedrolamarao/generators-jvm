bb0b69310d44fb49cb96248a09454b5f983e24b3
Pedro Lamarão <pedro.lamarao@prodist.com.br>
2023-02-27 17:48:54 -0300

    Remove incorrect obsolete source and target compatibility settings.

--- generators/build.gradle.kts
+++ generators/build.gradle.kts
@@ -5,10 +5,6 @@ plugins {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 }
-java {
-    sourceCompatibility = JavaVersion.VERSION_19
-    targetCompatibility = JavaVersion.VERSION_19
-}
 
 tasks.withType<JavaCompile>().configureEach {
     options.compilerArgs = listOf("--add-exports","java.base/jdk.internal.vm=br.dev.pedrolamarao.generators","--enable-preview")
