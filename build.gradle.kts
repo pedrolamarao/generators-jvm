08987032ce3d7e43fe6e3189785b21d6d16fe2ba
Pedro Lamarão <pedro.lamarao@gmail.com>
2023-03-22 13:01:04 -0300

    Update Gradle to 8.1-rc-1

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,11 +11,15 @@ subprojects {
     plugins.withType<JavaPlugin>().configureEach {
         extensions.getByType<JavaPluginExtension>().apply {
             toolchain {
-                languageVersion.set( JavaLanguageVersion.of(19) )
+                languageVersion.set( JavaLanguageVersion.of(20) )
             }
         }
         tasks.withType<Test>().configureEach {
             jvmArgs = listOf("--add-exports","java.base/jdk.internal.vm=br.dev.pedrolamarao.generators,ALL-UNNAMED","--enable-preview")
         }
     }
+}
+
+tasks.wrapper.configure {
+    gradleVersion = "8.1-rc-1"
 }
