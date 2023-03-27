24bccb9e48514cc426521b67663714a3ac00c36d
Pedro Lamarão <pedro.lamarao@gmail.com>
2023-03-27 12:55:39 -0300

    Test last line ending.

--- readers/line/src/test/java/br/dev/pedrolamarao/generators/line/LineGeneratorTest.java
+++ readers/line/src/test/java/br/dev/pedrolamarao/generators/line/LineGeneratorTest.java
@@ -14,18 +14,28 @@ public abstract class LineGeneratorTest
     abstract Generator<String> create (Reader reader, int buffer);
 
     @Test
-    public void test ()
+    public void test1 ()
     {
-        final var text =
-        """
-foo,bar,1.0, ,
-meh,duh,2.22,
-        """;
+        final var text = "foo,bar,1.0, ,\nmeh,duh,2.22,\nhmmm,hmmmmmmm,3.33\n";
 
         final var reader = create( new StringReader(text), 8 );
 
         assertEquals( "foo,bar,1.0, ,", reader.next() );
         assertEquals( "meh,duh,2.22,", reader.next() );
+        assertEquals( "hmmm,hmmmmmmm,3.33", reader.next() );
+        assertNull( reader.next() );
+    }
+
+    @Test
+    public void test2 ()
+    {
+        final var text = "foo,bar,1.0, ,\nmeh,duh,2.22,\nhmmm,hmmmmmmm,3.33";
+
+        final var reader = create( new StringReader(text), 8 );
+
+        assertEquals( "foo,bar,1.0, ,", reader.next() );
+        assertEquals( "meh,duh,2.22,", reader.next() );
+        assertEquals( "hmmm,hmmmmmmm,3.33", reader.next() );
         assertNull( reader.next() );
     }
 }
