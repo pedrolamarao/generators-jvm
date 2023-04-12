6bd2865edf39425ce61706318fef5cc8221f761d
Pedro Lamarão <pedro.lamarao@gmail.com>
2023-04-11 22:30:37 -0300

    Rename NextBenchmark to LineBenchmark.

--- readers/line/src/jmh/java/br/dev/pedrolamarao/generators/line/LineBenchmark.java
+++ readers/line/src/jmh/java/br/dev/pedrolamarao/generators/line/LineBenchmark.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 import java.util.stream.IntStream;
 
 @State(Scope.Benchmark)
-public class NextBenchmark
+public class LineBenchmark
 {
     @Param({"1024"})
     int length;
