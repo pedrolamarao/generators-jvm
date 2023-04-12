4247efe16e8284b63b98e36f380d7077ddd95748
Pedro Lamarão <pedro.lamarao@gmail.com>
2023-04-11 21:38:11 -0300

    Rename in preparation for the return of the buffered algorithm.

--- readers/line/src/main/java/br/dev/pedrolamarao/generators/line/LineParser.java
+++ readers/line/src/main/java/br/dev/pedrolamarao/generators/line/LineParser.java
@@ -15,36 +15,36 @@ public class LineParser
             return;
         }
 
-        final var buffer = new StringBuilder();
+        final var line = new StringBuilder();
 
         int byte_ = -1;
         while ((byte_ = reader.read()) != -1) {
             if (byte_ == '\n') {
-                consumer.accept( buffer.toString() );
-                buffer.setLength(0);
+                consumer.accept( line.toString() );
+                line.setLength(0);
             }
             else if (byte_ == '\r') {
                 if ((byte_ = reader.read()) == -1) break;
                 if (byte_ == '\n') {
-                    consumer.accept( buffer.toString() );
-                    buffer.setLength(0);
+                    consumer.accept( line.toString() );
+                    line.setLength(0);
                 }
                 else {
-                    buffer.append('\r');
-                    buffer.append((char)byte_);
+                    line.append('\r');
+                    line.append((char)byte_);
                 }
             }
             else {
-                buffer.append((char)byte_);
+                line.append((char)byte_);
             }
         }
 
-        if (! buffer.isEmpty()) consumer.accept( buffer.toString() );
+        if (! line.isEmpty()) consumer.accept( line.toString() );
     }
 
     static void parseWithMark (Reader reader, Consumer<String> consumer) throws IOException
     {
-        char[] buffer = new char[16];
+        char[] line = new char[16];
 
         reader.mark(Integer.MAX_VALUE);
         int count = 0;
@@ -52,24 +52,24 @@ public class LineParser
         while ((byte_ = reader.read()) != -1) {
             if (byte_ == '\n') {
                 final int length = count+1;
-                buffer = (buffer.length >= length) ? buffer : new char[length];
+                line = (line.length >= length) ? line : new char[length];
                 reader.reset();
-                final int read = reader.read(buffer,0,length);
+                final int read = reader.read(line,0,length);
                 assert read == length;
                 reader.mark(Integer.MAX_VALUE);
-                consumer.accept(new String(buffer,0,count));
+                consumer.accept(new String(line,0,count));
                 count = 0;
             }
             else if (byte_ == '\r') {
                 if ((byte_ = reader.read()) == -1) break;
                 if (byte_ == '\n') {
                     final int length = count+2;
-                    buffer = (buffer.length >= length) ? buffer : new char[length];
+                    line = (line.length >= length) ? line : new char[length];
                     reader.reset();
-                    final int read = reader.read(buffer,0,length);
+                    final int read = reader.read(line,0,length);
                     assert read == length;
                     reader.mark(Integer.MAX_VALUE);
-                    consumer.accept(new String(buffer,0,count));
+                    consumer.accept(new String(line,0,count));
                     count = 0;
                 }
                 else {
@@ -82,11 +82,11 @@ public class LineParser
         }
 
         if (count > 0) {
-            buffer = (buffer.length >= count) ? buffer : new char[count];
+            line = (line.length >= count) ? line : new char[count];
             reader.reset();
-            final int read = reader.read(buffer,0,count);
+            final int read = reader.read(line,0,count);
             assert read == count;
-            consumer.accept(new String(buffer,0,count));
+            consumer.accept(new String(line,0,count));
         }
     }
 
