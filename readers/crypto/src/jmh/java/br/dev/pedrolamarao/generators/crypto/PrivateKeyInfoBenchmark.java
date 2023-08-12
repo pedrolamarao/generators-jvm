5e8ef2cda01eb544d54c4a245266055f1a776ac9
Pedro Lamarão <pedro.lamarao@gmail.com>
2023-08-12 16:47:10 -0300

    Improve equivalence.

--- readers/crypto/src/jmh/java/br/dev/pedrolamarao/generators/crypto/PrivateKeyInfoBenchmark.java
+++ readers/crypto/src/jmh/java/br/dev/pedrolamarao/generators/crypto/PrivateKeyInfoBenchmark.java
@@ -3,14 +3,19 @@ package br.dev.pedrolamarao.generators.crypto;
 import br.dev.pedrolamarao.generators.ber.BerAbstractReader;
 import br.dev.pedrolamarao.generators.ber.BerRunnableReader;
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
 
 import java.io.ByteArrayInputStream;
 import java.security.KeyFactory;
 import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.SecureRandom;
+import java.security.interfaces.RSAPrivateKey;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.concurrent.TimeUnit;
 
+@BenchmarkMode(Mode.SampleTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
 public class PrivateKeyInfoBenchmark
 {
     static final SecureRandom secureRandom = new SecureRandom( new byte[0] );
@@ -33,20 +38,26 @@ public class PrivateKeyInfoBenchmark
     }
 
     @Benchmark
-    public PrivateKey abstractGenerator (Common it)
+    public PrivateKey abstractGenerator (Blackhole hole, Common it)
     {
-        return PrivateKeyInfoReader.read( new BerAbstractReader( new ByteArrayInputStream( it.data ) ) );
+        final var key = (RSAPrivateKey) PrivateKeyInfoReader.read( new BerAbstractReader( new ByteArrayInputStream( it.data ) ) );
+        hole.consume(key.getPrivateExponent());
+        return key;
     }
 
     @Benchmark
-    public PrivateKey jca (Common it) throws Exception
+    public PrivateKey jca (Blackhole hole, Common it) throws Exception
     {
-        return KeyFactory.getInstance("RSA").generatePrivate( new PKCS8EncodedKeySpec( it.data ) );
+        final var key = (RSAPrivateKey) KeyFactory.getInstance("RSA").generatePrivate( new PKCS8EncodedKeySpec( it.data.clone() ) );
+        hole.consume(key.getPrivateExponent());
+        return key;
     }
 
     @Benchmark
-    public PrivateKey runnableGenerator (Common it)
+    public PrivateKey runnableGenerator (Blackhole hole, Common it)
     {
-        return PrivateKeyInfoReader.read( new BerRunnableReader( new ByteArrayInputStream( it.data ) ) );
+        final var key = (RSAPrivateKey) PrivateKeyInfoReader.read( new BerRunnableReader( new ByteArrayInputStream( it.data ) ) );
+        hole.consume(key.getPrivateExponent());
+        return key;
     }
 }
